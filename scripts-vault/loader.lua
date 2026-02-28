4b0fc39b58e9e44e93ffbcf07dd0621ddeb7ba26
AlrecTofficial67 <239145936+AlrecTofficial67@users.noreply.github.com>
2026-03-01 06:06:21 +0800

    Update loader.lua

--- scripts-vault/loader.lua
+++ scripts-vault/loader.lua
@@ -3,20 +3,4 @@ getgenv().AlrectLoaded = true
 
 local URL = "https://prri-v14.vercel.app/lua/8f52fbb9e0902a389560f691"
 
-local code = game:HttpGet(URL)
-
-if not code or code == "" then
-    warn("[Alrect] Gagal download script")
-    return
-end
-
-local fn, err = load(code)
-if not fn then
-    warn("[Alrect] Load error: " .. tostring(err))
-    return
-end
-
-local ok, runErr = pcall(fn)
-if not ok then
-    warn("[Alrect] Runtime error: " .. tostring(runErr))
-end
+loadstring(game:HttpGet(URL))()
